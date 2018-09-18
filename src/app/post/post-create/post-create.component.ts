1381789f7ccd49cf301ef3fbc37086fdadc76d92
Akbar <abrly@yahoo.com>
2018-09-18 19:38:27 +0400

    simple

--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -20,7 +20,8 @@ export class PostCreateComponent implements OnInit {
     mode:number=0;
     pID:string;
 
-    pIdentify:string;
+    pIdentifys:string;
+    
 
     postInformation:Post={Id:'',Title:'',Content:'',ImagePath:''};
 
